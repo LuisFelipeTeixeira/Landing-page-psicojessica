35a7e6c63ea8e1aa2014ad367251e5f15591d8c7
luisftexeira <luisftexeira@gmail.com>
2024-07-25 22:27:59 -0300

    Mudancas no visual

--- assets/js/main-nav.js
+++ assets/js/main-nav.js
@@ -1,4 +1,4 @@
-/*===== EXPANDER MENU  =====*/
+/*===== EXPANDIR MENU  =====*/
 const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
         nav = document.getElementById(navId)
